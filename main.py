ba11972be77fc4d2607f633de5401874b9c27c7e
yilong100 <ywu737@aucklanduni.ac.nz>
2025-09-18 12:28:28 +1000

    draw traingle

--- main.py
+++ main.py
@@ -1,16 +1,24 @@
 import pygame
 from constants import *
+from player import Player
 
 def main():
     pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     infinite_loop_running = 1
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     while infinite_loop_running != 0:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
+        tick = clock.tick(60)
+        dt = tick/1000
+       
 
 
 if __name__ == "__main__":
